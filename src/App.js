ce62dadf7f0618ec283196a0e72979e9429d65ad
c <thomascavalcante77@gmail.com>
2023-01-03 19:14:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const weekday = ["Sun","Mon","Tues","Wednes","Thurs","Fri","Satur"];
+
+const date = new Date();
+let day = weekday[date.getDay()];
+
+const users = [
+	{
+		username: "sunday",
+		age: 15,
+		hobbies: ["Football", "Cricket"],
+	},
+	{
+		username: "monday",
+		age: 23,
+		hobbies: ["Reading", "Writing"],
+	},
+  {
+		username: "tuesday",
+		age: 15,
+		hobbies: ["Football", "Cricket"],
+	},
+	{
+		username: "wednesday",
+		age: 23,
+		hobbies: ["Reading", "Writing"],
+	},
+  {
+		username: "thrusday",
+		age: 15,
+		hobbies: ["Football", "Cricket"],
+	},
+	{
+		username: "friday",
+		age: 23,
+		hobbies: ["Reading", "Writing"],
+	},
+  {
+		username: "saturday",
+		age: 23,
+		hobbies: ["Reading", "Writing"],
+	},
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [subdomain, setSubDomain] = useState(null);
+	useEffect(() => {
+		const host = window.location.host; // gets the full domain of the app
+
+		const arr = host
+			.split(".")
+			.slice(0, host.includes("localhost") ? -1 : -2);
+		if (arr.length > 0) setSubDomain(arr[0]);
+	}, []);
+	const requestedUser = users.find((user) => user.username === subdomain);
+	return (
+		<div className="app">
+			{subdomain ? (
+				requestedUser ? (
+					<div>
+						<h1>Username</h1>
+						<h3>{requestedUser.username}</h3>
+						<h1>Age</h1>
+						<h3>{requestedUser.age}</h3>
+						<h1>Hobbies</h1>
+						<ul>
+							{requestedUser.hobbies.map((hobby) => (
+								<li key={hobby}>{hobby}</li>
+							))}
+						</ul>
+					</div>
+				) : (
+          <div> 
+            <h1>{day}day is today.</h1>
+            <article>
+              <p>This website changes its domain name depending on the day.<br></br>
+                <p>By Tom Cav, 2023</p>
+              </p>
+            </article>
+          </div>
+				)
+			) : (
+				<div>
+					{users.map((user) => (
+						<div>
+							<a
+								key={user.username}
+								href={
+									window.location.protocol +
+									"//" +
+									day +
+									"." +
+									window.location.host
+								}
+							>
+								{user.username}
+							</a>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
